b06e89b3d6dce40a792540751b5ddfc7588a1b13
opilane <opilane@HP-CZC2349HTD>
2023-12-11 10:26:58 +0200

    Add project files.

---
+++ Danila_Ivanov_lg23.py
@@ -0,0 +1,28 @@
+arv=float(input("Arv1: "))
+t=input("Tehe: ")
+arv=float(input("Arv2: "))
+vastus=eval(str(arv1)+t+str(arv2))
+print(str(arv1)+t+str(arv2)+"="+str(vastus))
+print(arv1,t,arv2,"=",vastus,sep="")        #разделитель
+print("{0}{1}{2}={3}".format(arv1,t,arv2,vastlus))
+
+
+
+
+
+
+
+
+
+
+
+
+#print ("Tere maailm!".center(75,"-"))
+#nimi=input("Mis on sinu nimi? ").capitalize() #python->Python
+#print("Tere "+nimi+"!")
+#print("Tere",nimi+"!")
+#print("Tere {0}!".format(nimi))
+#vanus=int(input("Kui vana sa oled"))           #"21"! не ровно=21
+#print("Tere {0}! Sa oled {1} aastat vana".format(nimi,vanus))
+#print("Muutuja nimi=", nimi, type(nimi))
+#print("Muutuja vanus=",vanus,type(vanus))
